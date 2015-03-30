746013acd6c6ecbdd633ed62f8f564ce438f7fac
Issaq Al-Ahmed <ial-ahmed@pivotal.io>
2015-03-30 14:33:06 -0700

    fix(linting) Fix quotes in search-input_spec [#91316206]
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- spec/search-input/search-input_spec.js
+++ spec/search-input/search-input_spec.js
@@ -11,7 +11,7 @@ describe('SearchInput', function() {
     React.unmountComponentAtNode(root);
   });
 
-  it("renders a form group with the search classes", function() {
+  it('renders a form group with the search classes', function() {
     expect('.form-group').toHaveClass('form-group-search');
 
     expect('.form-group input').toHaveClass('form-control');
@@ -20,35 +20,35 @@ describe('SearchInput', function() {
     expect('.form-group i').toHaveClass('fa-search');
   });
 
-  describe("when a placeholder is provided", function() {
+  describe('when a placeholder is provided', function() {
     beforeEach(function() {
       React.render((<SearchInput placeholder="Search here..."/>), root);
     });
 
-    it("renders the input with a placeholder", function() {
+    it('renders the input with a placeholder', function() {
       expect('.form-group input').toHaveAttr('placeholder', 'Search here...');
     });
   });
 
-  describe("when a className is provided", function() {
+  describe('when a className is provided', function() {
     beforeEach(function() {
       React.render((<SearchInput className="foo myClass"/>), root);
     });
 
-    it("adds the classes to the input", function() {
+    it('adds the classes to the input', function() {
       expect('.form-group input').toHaveClass('foo');
       expect('.form-group input').toHaveClass('myClass');
     });
   });
 
-  describe("when event handlers are provided", function() {
+  describe('when event handlers are provided', function() {
     var changeSpy;
     beforeEach(function() {
       changeSpy = jasmine.createSpy('change');
       React.render((<SearchInput onChange={changeSpy}/>), root);
     });
 
-    it("adds the handlers to the search input", function() {
+    it('adds the handlers to the search input', function() {
       $('input:eq(0)').simulate('change');
       expect(changeSpy).toHaveBeenCalled();
     });
